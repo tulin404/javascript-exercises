ad58a748197d2311cb189bcb8d2914e8fc6c7ce2
Tulio <221194504+tulin404@users.noreply.github.com>
2025-10-06 16:11:54 -0300

    Finished 10

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (first > last) {
+        [first, last] = [last, first]
+    }
+    if (typeof first !== "number" || typeof last !== "number"){
+        return "ERROR"
+    }
+    if (!Number.isInteger(first) || !Number.isInteger(last)) {
+        return "ERROR"
+    }
+    if (first < 0 || last < 0) {
+        return "ERROR"
+    }
+    
+    let sum = 0;
+    for (let i = first; i <= last; i++) {
+    sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
