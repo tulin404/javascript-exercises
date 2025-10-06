5663b9f9fb5a8a2c23f2abaab531e0cb7a9a6963
Tulio <221194504+tulin404@users.noreply.github.com>
2025-10-06 16:31:14 -0300

    Finished 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const roundedConverted = ((temp - 32) / 1.8).toFixed(1);
+  const convertedNum = parseFloat(roundedConverted);
+  return convertedNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const roundedConverted = (temp * 1.8 + 32).toFixed(1);
+  const convertedNum = parseFloat(roundedConverted);
+  return convertedNum;
 };
 
 // Do not edit below this line
