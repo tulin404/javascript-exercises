fcb1b3b48ec005c35777a97761cdd7f048f9ae5d
Tulio <221194504+tulin404@users.noreply.github.com>
2025-10-28 15:35:22 -0300

    Finished 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    correctedStr = str.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ").replace(/\s/g, '').toLowerCase();
+    reversedStr = correctedStr.split('').reverse().join('');
+    return correctedStr === reversedStr;
 };
 
 // Do not edit below this line
