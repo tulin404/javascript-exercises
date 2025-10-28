44048667ebcee227caeecec2ce716964fd7db1a5
Tulio <221194504+tulin404@users.noreply.github.com>
2025-10-28 15:14:04 -0300

    Finished 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr == []) {
+    return 0;
+  } else {
+    return arr.reduce((acc, current) => acc + current, 0);
+  };
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc * current);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  };
+  return fact;
 };
 
 // Do not edit below this line
