d7f09a4aeb2e716d8c314fed504986feeb0ce897
Tulio <221194504+tulin404@users.noreply.github.com>
2025-10-28 16:10:39 -0300

    Finished 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num !== "number"){
+        num = parseInt(num)
+    };
+    if (num < 0) {
+        return "OOPS"
+    };
+    if (num == 0){
+        return 0
+    } else {
+        let fib = [1, 1];
+        for (let i = fib.length; i < num; i++) {
+            let nextNum = fib[fib.length - 1] + fib[fib.length - 2];
+            fib.push(nextNum);
+        };
+        return fib[fib.length - 1];
+    };
 };
 
 // Do not edit below this line
