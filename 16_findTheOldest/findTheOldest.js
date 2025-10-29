4b5d5836751afb2634685773b12c39c1e1347078
Tulio <221194504+tulin404@users.noreply.github.com>
2025-10-29 16:13:17 -0300

    Finished 16. I'm really proud of my logic.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const today = new Date()
 
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+const findTheOldest = function(objArr) {
+for (const person of objArr) {
+        if (person.hasOwnProperty("yearOfDeath") === false) {
+            person.yearOfDeath = today.getFullYear();
+        };
+    };
+    let mappedObjArr = objArr.map(person => person.yearOfDeath - person.yearOfBirth);
+    let max = Math.max(...mappedObjArr);
+    let rigthPerson = objArr.find(person => (person.yearOfDeath - person.yearOfBirth) == max);
+    return rigthPerson;
 };
 
 // Do not edit below this line
